47581a473dd3814aca7feb30f3a6e973c04fa160
Janotelo <jagj102999@gmail.com>
2021-11-18 04:48:09 +0000

    Sytle CSS is now working properly

--- login.py
+++ login.py
@@ -1,18 +1,9 @@
 from flask import Flask, request, render_template, redirect
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__) # Relative Path ADD -> {static_url_path=''}
 
 @app.route("/", methods = ["GET","POST"])
 def user_login():
-    #reqUserLog = request.form['Username']
-    #reqPassLog = request.form['Password']
-    if request.method == 'POST':
-        if verify_cred(reqUserLog, reqPassLog):
-            return render_template("LoggendIn.html")
-        else:
-            error = 'Invalid username/password'
-    else:
-        error = 'Invalid Method'
     return render_template("login.html")
 
 if __name__== "__main__":
